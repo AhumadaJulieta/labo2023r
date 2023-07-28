cfe3fc0086f343f00ed5eaa80b7852c4d705ea81
AhumadaJulieta <127358388+AhumadaJulieta@users.noreply.github.com>
2023-07-28 09:27:03 -0300

    Add files via upload

---
+++ src/workflow-semillerio/A10_701_RUN_correr_workflow.r
@@ -0,0 +1,16 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+#source("~/labo2023r/src/workflow-semillerio/A2_711_CA_reparar_dataset.r")
+#source("~/labo2023r/src/workflow-semillerio/A2_721_DR_corregir_drifting.r")
+#source("~/labo2023r/src/workflow-semillerio/A2_731_FE_historia.r")
+source("~/labo2023r/src/workflow-semillerio/A10_741_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-semillerio/A10_751_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-semillerio/A10_771_ZZ_final_semillerio.r")
