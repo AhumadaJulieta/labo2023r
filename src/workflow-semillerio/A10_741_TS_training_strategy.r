1a7bdcb47c88c4125c56e23ac2e815bcb2baf42f
AhumadaJulieta <127358388+AhumadaJulieta@users.noreply.github.com>
2023-07-28 20:55:41 -0300

    Update A10_741_TS_training_strategy.r

--- src/workflow-semillerio/A10_741_TS_training_strategy.r
+++ src/workflow-semillerio/A10_741_TS_training_strategy.r
@@ -26,17 +26,17 @@ PARAM$final_train <- c(
 )
 
 PARAM$train$training <- c(
-  202102, 202101, 202012, 202011, 202010, 202009, 202008, 202007, 202006, 202005, 202004,
+  202012, 202011, 202010, 202009, 202008, 202007, 202006, 202005, 202004, 202003
   202002, 202001, 201912, 201911,
-  201910, 201909, 201908, 201907, 201906, 201905, 201904, 201903
+  201910, 201909, 201908, 201907, 201906, 201905, 201904, 201903, 201902
 )
 
-PARAM$train$validation <- c(202103)
-PARAM$train$testing <- c(202104, 202105, 202106)
+PARAM$train$validation <- c(202101)
+PARAM$train$testing <- c(202102, 202104, 202106)
 
 # Atencion  0.4  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
-PARAM$train$undersampling <- 0.1
+PARAM$train$undersampling <- 0.2
 
 PARAM$train$semilla <- 251033 # cambiar por su propia semilla  !!!
 
