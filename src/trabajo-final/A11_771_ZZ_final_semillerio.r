980d8c7f13913aae3bd6baef5eedb11e1f0c5a5a
AhumadaJulieta <127358388+AhumadaJulieta@users.noreply.github.com>
2023-07-30 12:42:22 -0300

    Update A11_771_ZZ_final_semillerio.r

--- src/trabajo-final/A11_771_ZZ_final_semillerio.r
+++ src/trabajo-final/A11_771_ZZ_final_semillerio.r
@@ -30,7 +30,7 @@ PARAM$modelos_rank <- c(10)
 # cantidad de semillas a utilizar en el semillerio,
 #  podrian ser 50 o 100 para mayor estabilidad
 #  semillerio <- 1  es el ZZ_final  tradicional
-PARAM$semillerio <- 50
+PARAM$semillerio <- 38  #inicialmente puse semillerio 50 pero el hibrido (archivo H5) lo realice con el archivo que se genero cuando iba por el semillerio 38 
 
 # se utiliza para generar el vector de  PARAM$semillerio  semillas
 PARAM$semilla_primos <- 251033
