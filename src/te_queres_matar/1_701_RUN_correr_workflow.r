c8958eca00c4f1446db2a0cdd4a1908b77586fba
AhumadaJulieta <127358388+AhumadaJulieta@users.noreply.github.com>
2023-07-31 09:28:57 -0300

    Create 1_701_RUN_correr_workflow.r

---
+++ src/te_queres_matar/1_701_RUN_correr_workflow.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow Semillerio
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+source("~/labo2023r/src/trabajo-final/A2_711_CA_reparar_dataset.r")
+source("~/labo2023r/src/trabajo-final/A2_721_DR_corregir_drifting.r")
+source("~/labo2023r/src/trabajo-final/A2_731_FE_historia.r")
+source("~/labo2023r/src/trabajo-final/A2_741_TS_training_strategy.r")
+source("~/labo2023r/src/trabajo-final/A2_751_HT_lightgbm.r")
+source("~/labo2023r/src/trabajo-final/A8_741_TS_training_strategy.r")
+source("~/labo2023r/src/trabajo-final/A8_751_HT_lightgbm.r")
+source("~/labo2023r/src/trabajo-final/A8_771_ZZ_final_semillerio.r")
+source("~/labo2023r/src/trabajo-final/A9_771_ZZ_final_semillerio.r")
+source("~/labo2023r/src/trabajo-final/H4_781_ZZ_semillerios_hibridacion.r")
